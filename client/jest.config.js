4dae7072d7a4c38a9d7707f6dc5e4e625d01abba
Viktor Puzyrev <v.puzyrev.pktm@gmail.com>
2022-05-19 21:57:03 +0400

    Edit Jest setup

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest",
-  setupFiles: ["./tests/setup.js"],
+  setupFiles: ["<rootDir>/tests/setup.js"],
+  transform: {
+    "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/tests/img-test-transformer.js",
+  },
 };
